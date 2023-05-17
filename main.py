32808b4658dbcb3d3b3d2e5f1c16ab7476eda278
AndreaScacchi <andreascacchi10@gmail.com>
2023-05-17 22:15:15 +0200

    match statement used to match the add case and perform the follow action: ask the user to enter a todo, open in read mode todos.txt file, reading multiple lines, close the file, append the todo enter to todos variable, open the file again in write mode to write multiple lines and close it

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 while True:
     user_action = input("Type add, show(or display), edit, complete(or delete), or exit: ")
     user_action = user_action.strip()
+
+    match user_action:
+        case 'add':
+            todo = input("Enter a todo: ") + "\n"
+
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
+
+            todos.append(todo)
+
+            file = open('todos.txt', 'w') # to write the file todos.txt
+            file.writelines(todos)
+            file.close()
