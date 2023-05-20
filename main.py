63d6e839891bdb01324112674cf778a2d61ae3cc
AndreaScacchi <andreascacchi10@gmail.com>
2023-05-20 16:24:25 +0200

    program improved

--- main.py
+++ main.py
@@ -1,92 +1,76 @@
 while True:
-    # ask the user to type a command
-    user_action = input("Type add, show(or display), edit, complete(or delete), or exit: ")
+    user_action = input("Type add, show(or display), edit, complete(or delete) or exit: ")
     user_action = user_action.strip()
 
-    match user_action: # check user action
+    if 'add' in user_action:
+        todo = user_action[4:] + '\n'   # list slicing: extract all the chars of the string starting from the fourth
 
-        case 'add':
-            todo = input("Enter a todo: ") + "\n"
+        # open the file
+        with open('todos.txt', 'r') as file:
+            todos = file.readlines()
 
-            # open the file
-            with open('todos.txt', 'r') as file:
-                todos = file.readlines()
+        todos.append(todo)
 
-            # append the todos to the file todos.txt
-            todos.append(todo)
+        with open('todos.txt', 'w') as file:
+            file.writelines(todos)
 
-            # open the file
-            with open('todos.txt', 'w') as file:
-                file.writelines(todos)
+    elif 'show' in user_action:
+        with open('todos.txt', 'r') as file:
+            todos = file.readlines()
 
-        case 'show' | 'display':
-            # open the file
-            with open('todos.txt', 'r') as file:
-                todos = file.readlines()
+        # clean up the list, remove the space between todos
+        # new_todos = []
+        # for item in todos:
+        #    new_item = item.strip('\n')
+        #    new_todos.append(new_item)
 
-            # clean up the list, remove the space between todos
-            # new_todos = []
-            # for item in todos:
-            #    new_item = item.strip('\n')
-            #    new_todos.append(new_item)
+        # to clean up the list you can also use list comprehensions
+        # new_todos = [item.strip('\n') for item in todos]
 
-            # to clean up the list you can also use list comprehensions
-            # new_todos = [item.strip('\n') for item in todos]
+        for index, item in enumerate(todos):
+            item = item.title()
 
-            # loop through the todos list and take the index and the todo(item)
-            for index, item in enumerate(todos):
-                item = item.title()
+            # to clean up the list you can also use the follow method inside the loop
+            item = item.strip('\n')
+            # print(index, '-', item) --> first method
 
-                # to clean up the list you can also use the follow method inside the loop
-                item = item.strip('\n')
+            # second method
+            row = f"{index + 1}-{item}"
+            print(row)  # --> second method
 
-                # print(index, '-', item) --> first method
+    elif 'edit' in user_action:
+        number = int(user_action[5:])
+        number -= 1
 
-                # second method
-                row = f"{index + 1}-{item}"
-                print(row) # --> second method
+        with open('todos.txt', 'r') as file:
+            todos = file.readlines()
 
-        case 'edit':
-            # ask the user to enter the number of the todo you want to edit
-            number = int(input("Number of the todo to edit: "))
-            number -= 1
+        new_todo = input("Enter new todo: ")
+        todos[number] = new_todo + '\n'
 
-            # open the file
-            with open('todos.txt', 'r') as file:
-                todos = file.readlines()
-            # ask the user to enter a new todo
-            new_todo = input("Enter new todo: ")
-            todos[number] = new_todo + '\n'
+        with open('todos.txt', 'w') as file:
+            file.writelines(todos)
 
-            # open the file
-            with open('todos.txt', 'w') as file:
-                file.writelines(todos)
+    elif 'complete' in user_action:
+        number = int(user_action[9:])
 
-        case 'complete' | 'delete':
-            # ask the user to enter the number of the todo you want to delete/complete
-            number = int(input("Number of the todo to complete: "))
-            
-            # open the file
-            with open('todos.txt', 'r') as file:
-                todos = file.readlines()
+        with open('todos.txt', 'r') as file:
+            todos = file.readlines()
 
-            # todo index you want to remove store inside the following variable
-            todo_index = number - 1
-            todo_to_remove = todos[todo_index].strip('\n')
-            todos.pop(number - 1)
+        todo_index = number - 1
+        todo_to_remove = todos[todo_index].strip('\n')
+        todos.pop(number - 1)
 
-            # open the file
-            with open('todos.txt', 'w') as file:
-                file.writelines(todos)
+        with open('todos.txt', 'w') as file:
+            file.writelines(todos)
 
-            # alert the user that the todo is removed 
-            message = f"Todo '{todo_to_remove}' was removed from the list!"
-            print(message)
+        message = f"Todo '{todo_to_remove}' was removed from the list!"
+        print(message)
 
-        case 'exit':
-            break # break the loop
+    elif 'exit' in user_action:
+        break
 
-        case _:
-            print("Hey, you enter an unknown command. Please select one action!")
+    else:
+        print("Command not valid")
 
 print("See you!🖐️")
